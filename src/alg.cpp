869bf17d07b5ae6dc2dd26ff0c878da5c6ea4d75
Sakhno Elizaveta <97175260+elizaveta08@users.noreply.github.com>
2022-03-20 18:55:16 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,7 @@ int countPairs1(int *arr, int len, int value) {
   for (int i = 0; i < len; i++)
       for (int j = i + 1; j < len; j++)
       if (i != j)
-          if (arr[i]+arr[j]) == value)
+          if (arr[i]+arr[j] == value)
             k++;
   return k / 2;
 }
@@ -13,7 +13,7 @@ int countPairs2(int *arr, int len, int value) {
   for (int i = 0; i < len - 1; i++) {
     for (int j = len - 1; j > i; j--) {
       if (i != j) {
-        if (arr[i]+arr[j]) == value) {
+        if (arr[i]+arr[j] == value) {
           k++;
         }
       }
